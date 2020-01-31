39e49dd7acdc1cbf54ea75cadc08fd3842755c6a
PatEnMer <patryk.nowak@gmail.com>
2020-01-31 10:54:08 +0100

    comst

--- 01_let_const/js/script.js
+++ 01_let_const/js/script.js
@@ -1,7 +1,17 @@
 (function() {
-	for (let i = 0; i < 10; i++) {
-		console.log(i);
-	}
-	// deklaracja zmiennej w pętli za pomocą let, pozwala uniknąć jej wykorzystania poza scopem
-	console.log('i: ' + i);
+	const a = 'ciąg znaków';
+	// const podobnie jak let jest dostępny w konkretnym scope, oraz nie jest hoistowany, jest to zmienna-stała: oznacza to, że nie można jej nadpisać
+	console.log(a);
+
+	// a = 'zzz';
+
+	const obj = {
+		name: 'Pat'
+	};
+
+	// obj = 'zasea';
+
+	obj.name = 'Pioter';
+	// wartość zmiennej zawartej w const jest możliwa do zmiany, ale nie można przypisywać do zmienniej czegoś zupełnie nowego
+	console.log(obj.name);
 })();
